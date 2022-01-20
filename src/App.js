5ae8486e49423503f5aef14af02fa75833b2f363
Edik Chekhanovsky <edichek12@hotmail.com>
2022-01-20 12:17:44 -0800

    Created home and faq pages

--- src/App.js
+++ src/App.js
@@ -2,14 +2,53 @@ import Chatbot from "./components/Chatbot";
 import "./axios-config.js";
 import { ThemeProvider } from "@emotion/react";
 import theme from "./theme.js";
-function App() {
+import SuggestedOptions from "./components/SuggestedOptions";
+import Home from "./components/Home";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <main id="app">
-        <Chatbot />
-      </main>
+    <ThemeProvider theme={theme}>  
+        <Router>
+            <div>
+                <nav>
+                    <div>
+                        <Link to="/">
+                            <button type="button">
+                                Home
+                            </button>
+                        </Link>
+                    </div>
+                    <div>
+                        <Link to="/chatbot">
+                            <button type="button">
+                                Chat
+                            </button>
+                        </Link>
+                    </div>
+                    <div>
+                        <Link to="/suggestions">
+                            <button type="button">
+                                Suggestions
+                            </button>
+                        </Link>
+                    </div>
+                </nav>
+
+                {/* A <Routes> looks through its children <Route>s and
+                    renders the first one that matches the current URL. */}
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/chatbot" element={<Chatbot/>}/>
+                    <Route path="/suggestions" element={<SuggestedOptions/>}/>
+                </Routes>
+            </div>
+        </Router>
     </ThemeProvider>
   );
 }
-
-export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+import '../style/home.css';
+
+export default function Home() {
+    var className = 'home bubble'
+
+    return (
+        <div className={className}>Howdy, welcome!</div>
+    );
+}
