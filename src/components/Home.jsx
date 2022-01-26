11051c5062d00a482c58f648cde93b50c919d216
lomccoy <lomccoy@calpoly.edu>
2022-01-26 15:44:04 -0800

    Temporary rebase until bootsrap is merged

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,13 @@
 export default function Home() {
+    //var className = 'home bubble'
+  //  <div className={className}>Howdy, welcome!
 
     return (
-        <div>
-            <div class="position-absolute top-50 start-50 translate-middle">
-                <button type="button" class="btn btn-primary">Poppy!</button>
-            </div>
-            <div class="alert alert-warning" role="alert">
-                This is an example 
-            </div>
-        </div>
+        <><div>
+            <p class="bubble">Chat with Poppy</p>
+        </div><div>
+                <p class="bubble">FAQ</p>
+            </div></>
+
     );
 }
