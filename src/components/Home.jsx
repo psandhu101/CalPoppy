2c43be3ada9e8363545dc7794873cb7b231c5697
lomccoy <lomccoy@calpoly.edu>
2022-01-25 23:37:21 -0800

    Added Bootstrap with Example

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,13 @@
-import '../style/home.css';
-
 export default function Home() {
-    var className = 'home bubble'
 
     return (
-        <div className={className}>Howdy, welcome!</div>
+        <div>
+            <div class="position-absolute top-50 start-50 translate-middle">
+                <button type="button" class="btn btn-primary">Poppy!</button>
+            </div>
+            <div class="alert alert-warning" role="alert">
+                This is an example 
+            </div>
+        </div>
     );
 }
