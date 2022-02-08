b792fd9b7c7fea353c86ee317a8952f4e941adf2
ebarruel <barruelevangeline.bchs@gmail.com>
2022-02-08 00:24:36 -0800

    edit whitespace

--- src/components/ChatComposer.jsx
+++ src/components/ChatComposer.jsx
@@ -5,48 +5,48 @@ import "../style/chatComposer.css";
 
 export default function ChatComposer({ onSend }) {
 
-  let textFieldRef = useRef(null);
+let textFieldRef = useRef(null);
 
-  // Takes the message from the content editable field and sends it out
-  function sendMessage() {
+// Takes the message from the content editable field and sends it out
+function sendMessage() {
     const emptyField = /^\s*$/g;
     if (
-      !textFieldRef.current ||
-      emptyField.test(textFieldRef.current.innerText)
+    !textFieldRef.current ||
+    emptyField.test(textFieldRef.current.innerText)
     )
-      return;
+    return;
     let message = textFieldRef.current.innerText.trim();
     textFieldRef.current.innerText = "";
     onSend(message);
-  }
+}
 
-  // Send Message on Enter Pressed
-  useEffect(() => {
+// Send Message on Enter Pressed
+useEffect(() => {
     const textField = textFieldRef.current;
     if (!textField) return;
     const onEnter = (e) => {
-      if (e.key === "Enter") {
+    if (e.key === "Enter") {
         e.preventDefault();
         sendMessage();
-      }
+    }
     };
     textField.addEventListener("keypress", onEnter);
     return () => textField.removeEventListener("keypress", onEnter);
-  });
+});
 
-  return (
+return (
     <div className="menuBarStyle ChatComposer">
-      <div className="contentStyle">
+    <div className="contentStyle">
         <div
-          className="txtFieldStyle"
-          ref={textFieldRef}
-          contentEditable="true"
+        className="txtFieldStyle"
+        ref={textFieldRef}
+        contentEditable="true"
         ></div>
         <button className="sendButtonStyle" onClick={() => sendMessage()}>
-          <Send size={20} />
-          <p className="buttonTextStyle">Send</p>
+        <Send size={20} />
+        <p className="buttonTextStyle">Send</p>
         </button>
-      </div>
     </div>
-  );
+    </div>
+);
 }
--- src/components/MessageBubble.jsx
+++ src/components/MessageBubble.jsx
@@ -1,38 +1,38 @@
-    /** @jsxImportSource @emotion/react */
-    import '../style/chatMsg.css';
-    import '../style/colors.css';
+/** @jsxImportSource @emotion/react */
+import '../style/chatMsg.css';
+import '../style/colors.css';
 
-    export default function MessageBubble({
-    text,
-    /* senderID: SENDER_USER when false, SENDER_BOT when true */
-    senderID,
-    showFeedback,
-    onFeedbackGiven,
-    timestamp,
-    })
-    
-    {
+export default function MessageBubble({
+text,
+/* senderID: SENDER_USER when false, SENDER_BOT when true */
+senderID,
+showFeedback,
+onFeedbackGiven,
+timestamp,
+})
 
-    return (
+{
 
-        /* wrapper div for messages */
-        <div className = {`msgWrapper ${ senderID ? "msgWrapperBot" : "msgWrapperUser" } `}>
-            {/* actual message text */}
-            <p className = {` msg ${ senderID ? "msgBot" : "msgUser" } `}>
-                {text}
-            </p>
-            <br></br>
-            {/* feedback buttons */}
-            { senderID && showFeedback && (
-                <>
-                <button type = "button" className = "feedbackIcon feedbackIconPos" onClick = {() => onFeedbackGiven(timestamp, true)}>
-                    <i class = "bi bi-hand-thumbs-up"></i>
-                </button>
-                <button type = "button" className = "feedbackIcon feedbackIconNeg" onClick = {() => onFeedbackGiven(timestamp, true)}>
-                    <i class="bi bi-hand-thumbs-down"></i>
-                </button>
-                </>
-            )}
-        </div>
-    );
-    }
+return (
+
+    /* wrapper div for messages */
+    <div className = {`msgWrapper ${ senderID ? "msgWrapperBot" : "msgWrapperUser" } `}>
+        {/* actual message text */}
+        <p className = {` msg ${ senderID ? "msgBot" : "msgUser" } `}>
+            {text}
+        </p>
+        <br></br>
+        {/* feedback buttons */}
+        { senderID && showFeedback && (
+            <>
+            <button type = "button" className = "feedbackIcon feedbackIconPos" onClick = {() => onFeedbackGiven(timestamp, true)}>
+                <i class = "bi bi-hand-thumbs-up"></i>
+            </button>
+            <button type = "button" className = "feedbackIcon feedbackIconNeg" onClick = {() => onFeedbackGiven(timestamp, true)}>
+                <i class="bi bi-hand-thumbs-down"></i>
+            </button>
+            </>
+        )}
+    </div>
+);
+}
