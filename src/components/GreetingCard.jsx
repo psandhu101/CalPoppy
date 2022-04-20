2ae9badfb019d64964d0ced1f490729a74d5ec15
ebarruel <barruelevangeline.bchs@gmail.com>
2022-04-20 11:51:25 -0700

    pull changes from master

--- src/components/GreetingCard.jsx
+++ src/components/GreetingCard.jsx
@@ -1,11 +1,11 @@
 /** @jsxImportSource @emotion/react */
 import '../style/greetingCard.css';
-import logoSrc from "../images/logo512.png"
 
 export default function GreetingCard() {
   const titleText = "Welcome to Swanton Poppy Chat!";
   const descriptionText = `Ask anything you want to know about the ranch!
   Check out some commonly asked questions by clicking the question mark above.`;
+  const logoSrc = `/logo512.png`;
   const logoAltText = `Swanton Poppy Logo`;
 
   const description = descriptionText.split("\n").map((text, i) => (
--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -3,6 +3,7 @@ import "../style/maps.css";
 
 export default function Maps() {
     return (
-        <iframe title="Google Map" class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2886.052013883088!2d-122.22390978493893!3d37.0429652798993!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808e5a5f1f697669%3A0x1bb4cbc2d3b0620!2sSwanton%20Pacific%20Ranch-Cal%20Poly!5e1!3m2!1sen!2sus!4v1644960004920!5m2!1sen!2sus" allowfullscreen="" loading="lazy"></iframe>
+        <iframe title="CalPoly SPR" class ="map" marginheight="0" marginwidth="0"
+        src="https://calpoly.maps.arcgis.com/apps/instant/basic/index.html?appid=154965d4fc4245e0ab96e94e63252f15"></iframe>
     )
 }
