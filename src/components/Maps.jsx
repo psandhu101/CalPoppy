1b8b80c4bbf32acafbdbccaa088b42e5c3ea65e6
ebarruel <barruelevangeline.bchs@gmail.com>
2022-04-23 11:52:53 -0700

    added temp descriptions for maps

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -3,7 +3,15 @@ import "../style/maps.css";
 
 export default function Maps() {
     return (
-        <iframe title="CalPoly SPR" class ="map" marginheight="0" marginwidth="0"
-        src="https://calpoly.maps.arcgis.com/apps/instant/basic/index.html?appid=154965d4fc4245e0ab96e94e63252f15"></iframe>
+        <div className="MapPage">
+            <h1 className="mapTitle">Swanton Pacific Ranch Maps</h1>
+            <text className="mapText">The interactive map below provides different views of the ranch. This includes several layers with different toggable map elements. A more comprehensive interactive map will be made available at a later date.</text>
+            <iframe title="CalPoly SPR" class ="map"
+            src="https://calpoly.maps.arcgis.com/apps/instant/basic/index.html?appid=154965d4fc4245e0ab96e94e63252f15"></iframe>
+            <hr className="divline"></hr>
+            <h1 className="mapTitle">Other Maps</h1>
+            <text className="mapText">Downloadable maps coming soon.</text>
+        </div>
+        
     )
 }
