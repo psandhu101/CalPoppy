2f246a877c91aa81e00958e57961f82b48c462bc
ebarruel <barruelevangeline.bchs@gmail.com>
2022-05-14 17:52:56 -0700

    adjusted whitespace

--- src/components/SpeechProcessing.jsx
+++ src/components/SpeechProcessing.jsx
@@ -78,20 +78,20 @@ async function synthesizeText(text, outputFile) {
 function streamingMicRecognize(encoding, sampleRateHertz, languageCode) {
     // [START speech_transcribe_streaming_mic]
     const recorder = require('node-record-lpcm16');
-  
+
     // Imports the Google Cloud client library
     const speech = require('@google-cloud/speech');
-  
+
     // Creates a client
     const client = new speech.SpeechClient();
-  
+
     /**
      * TODO(developer): Uncomment the following lines before running the sample.
      */
     // const encoding = 'Encoding of the audio file, e.g. LINEAR16';
     // const sampleRateHertz = 16000;
     // const languageCode = 'BCP-47 language code, e.g. en-US';
-  
+
     const request = {
       config: {
         encoding: encoding,
@@ -100,7 +100,7 @@ function streamingMicRecognize(encoding, sampleRateHertz, languageCode) {
       },
       interimResults: false, // If you want interim results, set this to true
     };
-  
+
     // Create a recognize stream
     const recognizeStream = client
       .streamingRecognize(request)
@@ -111,22 +111,22 @@ function streamingMicRecognize(encoding, sampleRateHertz, languageCode) {
             ? `Transcription: ${data.results[0].alternatives[0].transcript}\n`
             : '\n\nReached transcription time limit, press Ctrl+C\n'
         )
-      );
-  
+    );
+
     // Start recording and send the microphone input to the Speech API.
     // Ensure SoX is installed, see https://www.npmjs.com/package/node-record-lpcm16#dependencies
     recorder
-      .record({
-        sampleRateHertz: sampleRateHertz,
-        threshold: 0,
-        // Other options, see https://www.npmjs.com/package/node-record-lpcm16#options
-        verbose: false,
-        recordProgram: 'rec', // Try also "arecord" or "sox"
-        silence: '10.0',
-      })
-      .stream()
-      .on('error', console.error)
-      .pipe(recognizeStream);
+        .record({
+            sampleRateHertz: sampleRateHertz,
+            threshold: 0,
+            // Other options, see https://www.npmjs.com/package/node-record-lpcm16#options
+            verbose: false,
+            recordProgram: 'rec', // Try also "arecord" or "sox"
+            silence: '10.0',
+        })
+        .stream()
+        .on('error', console.error)
+        .pipe(recognizeStream);
 
     // TODO: stop when no input instead of with ctrl+c
     console.log('Listening, press Ctrl+C to stop.');
