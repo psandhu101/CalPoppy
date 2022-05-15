372cdffeb8c2272f80066537296929a3b120fa02
ebarruel <barruelevangeline.bchs@gmail.com>
2022-05-14 17:47:43 -0700

    switched stt method (stream instead of from file)

--- src/components/SpeechProcessing.jsx
+++ src/components/SpeechProcessing.jsx
@@ -67,15 +67,11 @@ async function synthesizeText(text, outputFile) {
  * at https://cloud.google.com/speech/docs.
  */
 
- async function syncRecognize(
-    filename,
-    encoding,
-    sampleRateHertz,
-    languageCode
-  ) {
-    // [START speech_transcribe_sync]
+ function streamingMicRecognize(encoding, sampleRateHertz, languageCode) {
+    // [START speech_transcribe_streaming_mic]
+    const recorder = require('node-record-lpcm16');
+  
     // Imports the Google Cloud client library
-    const fs = require('fs');
     const speech = require('@google-cloud/speech');
   
     // Creates a client
@@ -84,30 +80,46 @@ async function synthesizeText(text, outputFile) {
     /**
      * TODO(developer): Uncomment the following lines before running the sample.
      */
-    // const filename = 'Local path to audio file, e.g. /path/to/audio.raw';
     // const encoding = 'Encoding of the audio file, e.g. LINEAR16';
     // const sampleRateHertz = 16000;
     // const languageCode = 'BCP-47 language code, e.g. en-US';
   
-    const config = {
-      encoding: encoding,
-      sampleRateHertz: sampleRateHertz,
-      languageCode: languageCode,
-    };
-    const audio = {
-      content: fs.readFileSync(filename).toString('base64'),
-    };
-  
     const request = {
-      config: config,
-      audio: audio,
+      config: {
+        encoding: encoding,
+        sampleRateHertz: sampleRateHertz,
+        languageCode: languageCode,
+      },
+      interimResults: false, // If you want interim results, set this to true
     };
   
-    // Detects speech in the audio file
-    const [response] = await client.recognize(request);
-    const transcription = response.results
-      .map(result => result.alternatives[0].transcript)
-      .join('\n');
-    console.log('Transcription: ', transcription);
-    // [END speech_transcribe_sync]
+    // Create a recognize stream
+    const recognizeStream = client
+      .streamingRecognize(request)
+      .on('error', console.error)
+      .on('data', data =>
+        process.stdout.write(
+          data.results[0] && data.results[0].alternatives[0]
+            ? `Transcription: ${data.results[0].alternatives[0].transcript}\n`
+            : '\n\nReached transcription time limit, press Ctrl+C\n'
+        )
+      );
+  
+    // Start recording and send the microphone input to the Speech API.
+    // Ensure SoX is installed, see https://www.npmjs.com/package/node-record-lpcm16#dependencies
+    recorder
+      .record({
+        sampleRateHertz: sampleRateHertz,
+        threshold: 0,
+        // Other options, see https://www.npmjs.com/package/node-record-lpcm16#options
+        verbose: false,
+        recordProgram: 'rec', // Try also "arecord" or "sox"
+        silence: '10.0',
+      })
+      .stream()
+      .on('error', console.error)
+      .pipe(recognizeStream);
+  
+    console.log('Listening, press Ctrl+C to stop.');
+    // [END speech_transcribe_streaming_mic]
   }
