f7d720dd8734cf4cdce41f82670dc62fb451555e
ebarruel <barruelevangeline.bchs@gmail.com>
2022-06-01 15:19:00 -0700

    fix whitespace in chat composer

--- src/components/ChatComposer.jsx
+++ src/components/ChatComposer.jsx
@@ -9,52 +9,52 @@ import { synthesizeText } from "./SpeechProcessing";
 
 export default function ChatComposer({ onSend }) {
 
-let textFieldRef = useRef(null);
+    let textFieldRef = useRef(null);
 
-// Takes the message from the content editable field and sends it out
-function sendMessage() {
-    const emptyField = /^\s*$/g;
-    if (
-    !textFieldRef.current ||
-    emptyField.test(textFieldRef.current.innerText)
-    )
-    return;
-    let message = textFieldRef.current.innerText.trim();
-    textFieldRef.current.innerText = "";
-    onSend(message);
-}
-
-// Send Message on Enter Pressed
-useEffect(() => {
-    const textField = textFieldRef.current;
-    if (!textField) return;
-    const onEnter = (e) => {
-    if (e.key === "Enter") {
-        e.preventDefault();
-        sendMessage();
+    // Takes the message from the content editable field and sends it out
+    function sendMessage() {
+        const emptyField = /^\s*$/g;
+        if (
+        !textFieldRef.current ||
+        emptyField.test(textFieldRef.current.innerText)
+        )
+        return;
+        let message = textFieldRef.current.innerText.trim();
+        textFieldRef.current.innerText = "";
+        onSend(message);
     }
-    };
-    textField.addEventListener("keypress", onEnter);
-    return () => textField.removeEventListener("keypress", onEnter);
-});
 
-return (
-    <div className="menuBarStyle ChatComposer">
-    <div className="contentStyle">
-        <div
-            className="scrollableY txtFieldStyle"
-            ref={textFieldRef}
-            contentEditable="true"
-        ></div>
-        <button className="sendButtonStyle" onClick={() => sendMessage()}>
-            <Send size={20} />
-            <p className="buttonTextStyle">Send</p>
-        </button>
-        <button className="sendButtonStyle">
-            <Mic size={20} />
-            <p className="buttonTextStyle">Speak</p>
-        </button>
-    </div>
-    </div>
-);
+    // Send Message on Enter Pressed
+    useEffect(() => {
+        const textField = textFieldRef.current;
+        if (!textField) return;
+        const onEnter = (e) => {
+        if (e.key === "Enter") {
+            e.preventDefault();
+            sendMessage();
+        }
+        };
+        textField.addEventListener("keypress", onEnter);
+        return () => textField.removeEventListener("keypress", onEnter);
+    });
+
+    return (
+        <div className="menuBarStyle ChatComposer">
+        <div className="contentStyle">
+            <div
+                className="scrollableY txtFieldStyle"
+                ref={textFieldRef}
+                contentEditable="true"
+            ></div>
+            <button className="sendButtonStyle" onClick={() => sendMessage()}>
+                <Send size={20} />
+                <p className="buttonTextStyle">Send</p>
+            </button>
+            <button className="sendButtonStyle">
+                <Mic size={20} />
+                <p className="buttonTextStyle">Speak</p>
+            </button>
+        </div>
+        </div>
+    );
 }
