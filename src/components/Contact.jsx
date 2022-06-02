fdb9de1154fae4a353db2050c02e0dfc5218b89f
ebarruel <barruelevangeline.bchs@gmail.com>
2022-06-02 11:46:15 -0700

    add cal poppy libguide

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,5 @@
 import { contactPoppy, contactJMS } from "./contactTxt"
+import { libguide } from "./contactTxt";
 
 const Contact = () => {
     return (
@@ -8,6 +9,7 @@ const Contact = () => {
 
             <h1>Cal Poppy Program</h1>
             { contactJMS[0] } <br /><br /> { contactJMS[1] }
+            <a href={`${libguide.calpoppy}`} target="_blank">{libguide.calpoppy}</a>
              
         </div>
     )
