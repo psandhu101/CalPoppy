a0b6c06b344bfc973af5aaee00d965a6308982ff
ebarruel <barruelevangeline.bchs@gmail.com>
2022-06-02 15:22:04 -0700

    edit 404 page

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,9 +1,9 @@
 const PageNotFound = () => {
     return (
-        <div>
+        <div style={{ margin: "2.5%" }}>
             <h1>404</h1>
             <h2>Page Not Found</h2>
-            We are currently working on this functionality!
+            We are currently working on this functionality! Please check back at a later time.
         </div>
     )
 }
