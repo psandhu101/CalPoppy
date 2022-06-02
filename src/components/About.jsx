25a09a54a42565d4d3e799d4473a377d01901e29
ebarruel <barruelevangeline.bchs@gmail.com>
2022-06-02 14:01:37 -0700

    pulled master frontend

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,8 @@
 
-import suggestions from "./faqs";
-import {aboutSPR, aboutCreators, helpMessage} from "./faqs";
+import {suggestions, aboutSPR, aboutCreators, aboutHayes, aboutKurfess, disclaimer} from "./aboutTxt";
 import '../style/about.css';
+import hayes from "../images/hayes_grey.jpeg";
+import kurfess from "../images/kurfess_franz.jpeg";
 
 const About = () => {
   return (
@@ -40,19 +41,33 @@ const About = () => {
               <h4 class="card-title">About the Creators</h4>
               <h6><br/>{aboutCreators[0]}<p/>
                   {aboutCreators[1]}<br/>
-                  {aboutCreators[2]}</h6>
+                  {aboutCreators[2]}
+              </h6>
+
+              <h5>
+                <br/>Dr. Grey Hayes<br/>
+              </h5>
+              <img src={hayes} width="50%" /><br/>
+              {aboutHayes[1]}
+              <h6>
+                <br/>{aboutHayes[0]}
+              </h6>
+
+              <h5>
+                <br/>Dr. Franz J. Kurfess<br/>
+              </h5>
+              <img src={kurfess} width="50%" /><br/>
+              <h6>
+                <br/>{aboutKurfess[0]}<br/>{aboutKurfess[1]}
+              </h6>
             </div> 
           </div>
         </div>
         <div class="col" align="center">
           <div class="bubble">
             <div class="card-body">
-              <h4 class="card-title">Want to Help?</h4>
-              <h6><br/>{helpMessage[0]}<br/>
-                  {helpMessage[1]}<p/>
-                  {helpMessage[2]}<br/>
-                  {helpMessage[3]}
-              </h6>
+              <h4 class="card-title">Disclaimer</h4>
+              <h6>{ disclaimer }</h6>
             </div> 
           </div> 
         </div>
