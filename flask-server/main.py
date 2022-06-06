6d3472db07080a553bf46067bfcc46d6ee622703
lomccoy <lomccoy@calpoly.edu>
2022-06-06 13:24:15 -0700

    Updated main.py

--- flask-server/main.py
+++ flask-server/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, make_response
 import sys
 sys.path.append('./KGQA')
+
 app = Flask(__name__)
 
 
@@ -8,7 +9,7 @@ app = Flask(__name__)
 @app.route("/api_call", methods=["POST"])
 def api_call():
     user_query = request.get_json()
-    resp = {"sentences": str(KGQA.main.main(user_query))}
+    resp = {"sentences": str(KGQA.main.main(["Chatbot query", user_query]))}
     return resp
     
 
