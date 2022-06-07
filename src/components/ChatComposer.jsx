6ab9eeac23ebcd22e96a2551f910d7a79b78798b
ebarruel <barruelevangeline.bchs@gmail.com>
2022-06-07 09:38:54 -0700

    fix classname (scrollablyY to scrollableY)

--- src/components/ChatComposer.jsx
+++ src/components/ChatComposer.jsx
@@ -97,7 +97,7 @@ export default function ChatComposer({ onSend }) {
                     }
                     {onscreenKey && (
                         <input
-                            className="scrollablyY txtFieldStyle"
+                            className="scrollableY txtFieldStyle"
                             value={input}
                             onChange={onChangeInput}
                         />
