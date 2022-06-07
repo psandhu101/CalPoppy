0f4822ea6f80a264251ce1611ff1e030f77a13e5
ebarruel <barruelevangeline.bchs@gmail.com>
2022-06-07 09:24:39 -0700

    can now send input and clear input field

--- src/components/ChatComposer.jsx
+++ src/components/ChatComposer.jsx
@@ -10,45 +10,57 @@ import 'react-simple-keyboard/build/css/index.css';
 
 export default function ChatComposer({ onSend }) {
 
-    const [keyboard, setKeyboard] = useState(false);
-    state = {
-        layoutName: "default",
-        input: ""
-    };
-
     let textFieldRef = useRef(null);
 
-    // keyboard functions (straight from documentation)
-    onChange = input => {
-        this.setState({ input });
+    // keyboard functionality
+    const [onscreenKey, setOnscreenKey] = useState(false);
+    const [input, setInput] = useState("");
+    const [layout, setLayout] = useState("default");
+    const keyboard = useRef();
+
+    const onChange = input => {
+        setInput(input);
+        // textFieldRef.current.innerText = input;
         console.log("Input changed", input);
     };
 
-    onKeyPress = button => {
+    const handleShift = () => {
+        const newLayoutName = layout === "default" ? "shift" : "default";
+        setLayout(newLayoutName);
+    };
+
+    const onKeyPress = (button, input) => {
         console.log("Button pressed", button);
 
         /**
          * If you want to handle the shift and caps lock buttons
          */
-        if (button === "{shift}" || button === "{lock}") this.handleShift();
+        if (button === "{shift}" || button === "{lock}") handleShift();
+        if (button === "{enter}") sendMessage(input);
     };
 
-    handleShift = () => {
-        const layoutName = this.state.layoutName;
-
-        this.setState({
-            layoutName: layoutName === "default" ? "shift" : "default"
-        });
+    const onChangeInput = event => {
+        const input = event.target.value;
+        setInput(input);
+        keyboard.current.setInput(input);
     };
 
     // Takes the message from the content editable field and sends it out
     function sendMessage() {
         const emptyField = /^\s*$/g;
+        if (onscreenKey) {
+           console.log("input (send msg): ", input); 
+        }
+        console.log("text field (send msg): ", textFieldRef);
         if (
             !textFieldRef.current ||
             emptyField.test(textFieldRef.current.innerText)
-        )
-        return;
+        ) {
+            onSend(input);
+            console.log("keyboard:", keyboard);
+            setInput("");
+            return;
+        }
         let message = textFieldRef.current.innerText.trim();
         textFieldRef.current.innerText = "";
         onSend(message);
@@ -57,28 +69,41 @@ export default function ChatComposer({ onSend }) {
     // Send Message on Enter Pressed
     useEffect(() => {
         const textField = textFieldRef.current;
-        if (!textField) return;
+        console.log("text field: ", textField);
+        if (!textField) {return};
         const onEnter = (e) => {
             if (e.key === "Enter") {
                 e.preventDefault();
                 sendMessage();
             }
         };
-        textField.addEventListener("keypress", onEnter);
-        return () => textField.removeEventListener("keypress", onEnter);
+
+        if (!onscreenKey) {
+            textField.addEventListener("keypress", onEnter);
+            return () => textField.removeEventListener("keypress", onEnter);
+        }
+        return;
     });
 
     return (
         <div>
             <div className="menuBarStyle ChatComposer">
-                <div className={ `contentStyle` }>
-                    <div
-                        className="scrollableY txtFieldStyle"
-                        ref={textFieldRef}
-                        contentEditable="true"
-                    >
-                    </div>
-                    <button className="sendButtonStyle" onClick={() => setKeyboard(!keyboard)}>
+                <div className="contentStyle">
+                    {!onscreenKey && 
+                        <div
+                            className="scrollableY txtFieldStyle"
+                            ref={textFieldRef}
+                            contentEditable="true"
+                        ></div>
+                    }
+                    {onscreenKey && (
+                        <input
+                            className="scrollablyY txtFieldStyle"
+                            value={input}
+                            onChange={onChangeInput}
+                        />
+                    )}
+                    <button className="sendButtonStyle" onClick={() => setOnscreenKey(!onscreenKey)}>
                         <i class="bi bi-keyboard"></i>
                     </button>
                     <button className="sendButtonStyle" onClick={() => sendMessage()}>
@@ -88,10 +113,12 @@ export default function ChatComposer({ onSend }) {
                 </div>
             </div>
 
-            {keyboard && (
+            {onscreenKey && (
                 <Keyboard
-                    // onChange={this.onChange}
-                    // onKeyPress={this.onKeyPress}
+                    keyboardRef={r => (keyboard.current = r)}
+                    layoutName={layout}
+                    onChange={onChange}
+                    onKeyPress={onKeyPress}
                 />
             )}
             
