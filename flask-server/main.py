922ca0589c535d6349afe00aa81a2192f51dfcb4
lomccoy <lomccoy@calpoly.edu>
2022-06-07 14:11:33 -0700

    Restored Known Good Main.py

--- flask-server/main.py
+++ flask-server/main.py
@@ -3,8 +3,6 @@ import sys
 sys.path.append('./KGQA')
 from KGQA import statement_intake
 
-
-
 app = Flask(__name__)
 
 
@@ -12,8 +10,13 @@ app = Flask(__name__)
 @app.route("/api_call", methods=["POST"])
 def api_call():
     user_query = request.get_json()
+    print("In API CALL: ", user_query)
+    #resp = make_response(jsonify({"sentences": str(statement_intake.main(["Chatbot query", user_query]))}), 400)
+    #resp.headers["Access-Control-Allow-Origin"] = "*"
+    #resp = {"sentences": "ok, right spot"}
     resp = {"sentences": str(statement_intake.main(["Chatbot query", user_query]))}
-
+    #test = str(statement_intake.main(["Chatbot query", user_query]))
+    #print("Test API Result:", test)
     return resp
     
 
