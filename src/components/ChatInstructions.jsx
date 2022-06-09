4373af848305d53330bdb8231e53aa2e566d1f15
lomccoy <49251087+lomccoy@users.noreply.github.com>
2022-06-09 13:38:00 -0700

    Update ChatInstructions.jsx

--- src/components/ChatInstructions.jsx
+++ src/components/ChatInstructions.jsx
@@ -12,7 +12,7 @@ export default function ChatInstructions() {
             <h4>How to use the feedback feature:</h4>
             Please use the thumbs up/thumbs down buttons below each Poppy response to let us 
             know whether or not Poppy answered your question appropriately! <p/>
-            <h4>Learn about using the <Link to="/about"  style={{ color: '#FFF' }}> Map feature </Link> </h4>
+            <h4>Learn about using the Map features on the <Link to="/about"  style={{ color: '#FFF' }}> Maps page </Link> </h4>
         </div>
     </div>
   );
