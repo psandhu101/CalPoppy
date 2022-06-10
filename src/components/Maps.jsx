b68b4f0faa7d8f689846bfff97bf99cd16375e16
ebarruel <barruelevangeline.bchs@gmail.com>
2022-06-10 09:02:46 -0700

    edit line spacing

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -23,11 +23,12 @@ export default function Maps() {
             <iframe title="CalPoly SPR" class="map"
             src="https://calpoly.maps.arcgis.com/apps/instant/basic/index.html?appid=154965d4fc4245e0ab96e94e63252f15"></iframe>
 
-            <br/><br/>
+            <br/>
             <text className="mapText">Here is an additional interactive map if the ArcGIS map does not load.</text>
+            <br/>
             <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d43564.99188828965!2d-122.25948581283794!3d37.06328194401197!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x1bb4cbc2d3b0620!2sSwanton%20Pacific%20Ranch-Cal%20Poly!5e0!3m2!1sen!2sus!4v1654876300117!5m2!1sen!2sus" class="map" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             
-            <br/><br/>
+            <br/>
             <hr className="divline"></hr>
             <h1 className="mapTitle">Other Maps</h1>
             <text className="mapText">More maps coming soon.</text>
