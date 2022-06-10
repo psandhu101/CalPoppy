04256224181c3012186ca01500792b58a1a96366
ebarruel <barruelevangeline.bchs@gmail.com>
2022-06-10 08:38:23 -0700

    edit map description and todo: add google map embed

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -19,9 +19,12 @@ export default function Maps() {
                     post-fire
                 */}
             <h1 className="mapTitle">Swanton Pacific Ranch Maps</h1>
-            <text className="mapText">The interactive map below provides different views of the ranch. This includes several layers with different toggable map elements. A more comprehensive interactive map will be made available at a later date.</text>
+            <text className="mapText">The interactive map below provides different views of the ranch. It may take a few seconds to load. You can toggle different layers and map elements by clicking INSERT LAYER BUTTON. A map legend is available here INSERT LEGEND BUTTON. A more comprehensive interactive map will be made available at a later date.</text>
             <iframe title="CalPoly SPR" class ="map"
             src="https://calpoly.maps.arcgis.com/apps/instant/basic/index.html?appid=154965d4fc4245e0ab96e94e63252f15"></iframe>
+            {/* 
+              * embed google map again
+              */}
             <hr className="divline"></hr>
             <h1 className="mapTitle">Other Maps</h1>
             <text className="mapText">More maps coming soon.</text>
