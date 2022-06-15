d638ae7c2fe34cb331172c2821492dbd79253840
lomccoy <lomccoy@calpoly.edu>
2022-06-14 19:26:27 -0700

    Added and linked Suggested questions

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,15 @@
 
-import {suggestions, aboutSPR, aboutCreators, aboutHayes, aboutKurfess, disclaimer} from "./aboutTxt";
+import {suggestions, suggestionsAnswers, aboutSPR, aboutCreators, aboutHayes, aboutKurfess, disclaimer} from "./aboutTxt";
 import '../style/about.css';
 import hayes from "../images/hayes_grey.jpeg";
 import kurfess from "../images/kurfess_franz.jpeg";
 import { suggested } from "./Chatbot";
 import { libguide } from "./contactTxt";
+import { useNavigate } from 'react-router-dom';
+
 
 const About = () => {
+  const navigate = useNavigate()
   return (
     <div class="container">
       <div class="row text-white center-block">
@@ -16,12 +19,12 @@ const About = () => {
               <h4 class="card-title">Suggested Questions</h4>
               <h6>Click on any of the questions to ask Poppy!</h6>
               <div class="btn-group-vertical">
-                <button onClick={() => suggested(suggestions[0])} type="button" class="roundbutton">{suggestions[0]}</button>
-                <button onClick="sendMessage(suggestions[1])" type="button" class="roundbutton">{suggestions[1]}</button>
-                <button onClick="sendMessage(suggestions[2])" type="button" class="roundbutton">{suggestions[2]}</button>
-                <button onClick="sendMessage(suggestions[3])" type="button" class="roundbutton">{suggestions[3]}</button>
-                <button onClick="sendMessage(suggestions[4])" type="button" class="roundbutton">{suggestions[4]}</button>
-                <button onClick="sendMessage(suggestions[5])" type="button" class="roundbutton">{suggestions[5]}</button>
+                <button onClick={() =>{navigate('/chatbot'); suggested(suggestions[0], suggestionsAnswers[0]);}} type="button" class="roundbutton">{suggestions[0]}</button>
+                <button onClick={() =>{navigate('/chatbot'); suggested(suggestions[1], suggestionsAnswers[1]);}} type="button" class="roundbutton">{suggestions[1]}</button>
+                <button onClick={() =>{navigate('/chatbot'); suggested(suggestions[2], suggestionsAnswers[2]);}} type="button" class="roundbutton">{suggestions[2]}</button>
+                <button onClick={() =>{navigate('/chatbot'); suggested(suggestions[3], suggestionsAnswers[3]);}} type="button" class="roundbutton">{suggestions[3]}</button>
+                <button onClick={() =>{navigate('/chatbot'); suggested(suggestions[4], suggestionsAnswers[4]);}} type="button" class="roundbutton">{suggestions[4]}</button>
+                <button onClick={() =>{navigate('/chatbot'); suggested(suggestions[5], suggestionsAnswers[5]);}} type="button" class="roundbutton">{suggestions[5]}</button>
               </div>
             </div>
           </div>
